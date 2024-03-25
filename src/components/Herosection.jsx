b765d9ce0b50f52d1ac8d43edfc287b1babe76ce
saurabhdhebe2 <108879055+saurabhdhebe2@users.noreply.github.com>
2024-03-25 12:03:18 +0530

    bug fix

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,19 +1,33 @@
 // components/HeroSection.js
 import Image from 'next/image';
 import Link from 'next/link';
+import {
+  FaInstagram,
+  FaLinkedin,
+  FaYoutube,
+  FaVimeo,
+  FaEnvelope,
+} from 'react-icons/fa';
 
 const HeroSection = () => {
   return (
     <div className="relative p-0 m-0">
       {/* Image with Next.js Image Component */}
-      <Image
-        src="/assets/images/hero-image.jpeg" // Path to your image file
-        alt="Hero Image"
-        width={1920} // Adjust width and height as per your image dimensions
-        height={1080}
-      />
+
+      <video
+        className="opacity-100"
+        width="1920"
+        height="1080"
+        autoPlay
+        muted
+        loop
+        preload="none"
+      >
+        <source src="/assets/videos/hero-video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       {/* Content overlay */}
-      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-white p-8">
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-white p-8 bg-black bg-opacity-90">
         <h1 className="text-4xl md:text-6xl text-center font-bold mb-6">
           Shubham Dhebe
         </h1>
@@ -28,6 +42,52 @@ const HeroSection = () => {
           <button className="bg-primary-transparent  text-white px-4 py-4 border-button border-2 rounded-md">
             <Link href="/work"> PORTFOLIO</Link>
           </button>
+        </div>
+
+        <div className="flex space-x-4 mt-36">
+          {/* Instagram */}
+          <Link
+            href="https://www.instagram.com/your-instagram"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className="text-gray-300 border-secondary text-4xl hover:text-gray-400" />
+          </Link>
+          {/* LinkedIn */}
+          <Link
+            href="https://www.linkedin.com/in/your-linkedin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin className="text-gray-300 text-4xl  hover:text-gray-400" />
+          </Link>
+          {/* YouTube */}
+          <Link
+            href="https://www.youtube.com/your-youtube"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaYoutube className="text-gray-300 text-4xl  hover:text-gray-400" />
+          </Link>
+          {/* Vimeo */}
+          <Link
+            href="https://vimeo.com/your-vimeo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaVimeo className="text-gray-300 text-4xl  hover:text-gray-400" />
+          </Link>
+          {/* Gmail */}
+          <Link
+            href="mailto:your.email@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaEnvelope className="text-gray-300 text-4xl  hover:text-gray-400" />
+          </Link>
+        </div>
+        <div className="flex space-x-4 mt-6">
+          {/* Add more icons and links as needed */}
         </div>
       </div>
     </div>
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,14 +1,8 @@
 import { useState } from 'react';
 
 const VideoDetail = ({ video }) => {
-  const [showDetails, setShowDetails] = useState(false);
-
   return (
-    <div
-      className="relative group"
-      onMouseEnter={() => setShowDetails(true)}
-      onMouseLeave={() => setShowDetails(false)}
-    >
+    <div className="relative">
       <iframe
         id={`video-${video.id}`}
         className="w-full h-60"
@@ -19,14 +13,6 @@ const VideoDetail = ({ video }) => {
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
       ></iframe>
-      <div
-        className={`absolute inset-0 flex flex-col justify-center items-center bg-gray-900 bg-opacity-75 text-white transition-opacity duration-300 ${
-          showDetails ? 'opacity-100' : 'opacity-0'
-        }`}
-      >
-        <h2 className="text-lg font-semibold mb-2">{video.title}</h2>
-        <p className="text-gray-200 mb-4">{video.description}</p>
-      </div>
     </div>
   );
 };
