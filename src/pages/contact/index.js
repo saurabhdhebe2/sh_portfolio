e9e3021455c2330bd98f27db0b09893786476d22
saurabhdhebe2 <108879055+saurabhdhebe2@users.noreply.github.com>
2024-03-29 13:19:00 +0530

    bug fix

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -3,12 +3,12 @@ import RootLayout from '@/components/Layout';
 
 const Contact = () => {
   return (
-    <RootLayout className="bg-gray-100">
+    <RootLayout isNav={true} className="bg-gray-100">
       <div className="py-36">
         <div className="flex flex-col items-center justify-center">
           <h2 className="text-2xl text-cyan-300 font-bold mb-4">Contact</h2>
           <div className="h-10 bg-gray-500 w-px my-4"></div>
-          <h2 className="text-4xl font-bold mb-4">sdhebe123@gmail.com</h2>
+          <h2 className="text-3xl font-bold mb-4 px-2">sdhebe123@gmail.com</h2>
         </div>
         <div className="mt-12">
           <GetInTouchForm />
