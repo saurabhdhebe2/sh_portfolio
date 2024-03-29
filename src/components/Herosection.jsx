4628e6f5d593d02be00ba1d208d4bc420d41cc4d
saurabhdhebe2 <108879055+saurabhdhebe2@users.noreply.github.com>
2024-03-29 17:24:39 +0530

    bug fix

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -33,7 +33,7 @@ const HeroSection = () => {
         </video>
       )}
       {/* Content overlay */}
-      <div className="bg-black bg-opacity-60 w-auto  absolute inset-0 object-cover  flex flex-col justify-center items-center text-white p-4 md:p-8 md:bg-black md:bg-opacity-90">
+      <div className="bg-black bg-opacity-60 w-auto  absolute inset-0 object-cover  flex flex-col justify-center items-center text-white p-4 mt-[-100px] md:p-8 md:mt-0 md:bg-black md:bg-opacity-90">
         {/* <div className="absolute sm:pt-20 py-[500px] md:pt-20 py-[500px]  lg:top-0 left-0 w-full h-full lg:py-[485px] px-32 flex flex-col justify-center items-center text-white p-4 md:p-8 bg-black bg-opacity-90 0"> */}
         <p className="font-bebas-neue text-md text-center font-extralight tracking-widest">
           FILMMAKER | PHOTOGRAOHER
