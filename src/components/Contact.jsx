96cefe9d4f021a38ef3df402a599558258824140
saurabhdhebe2 <108879055+saurabhdhebe2@users.noreply.github.com>
2024-03-30 22:00:30 +0530

    bug fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -93,10 +93,7 @@ const GetInTouchForm = () => {
               />
             </div>
             <div className="flex-grow">
-              <label
-                htmlFor="email"
-                className="sm:mt-2 md:mt-0 block mb-2 font-semibold"
-              >
+              <label htmlFor="email" className="block mb-2 font-semibold">
                 EMAIL:
               </label>
               <input
