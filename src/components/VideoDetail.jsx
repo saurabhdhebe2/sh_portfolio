426755694395e88729e76d5cabac54fd8f7dcf34
Saurabh <saurabh@Saurabhs-MacBook-Air.local>
2024-12-22 16:26:34 +0530

    fixgit

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -2,10 +2,10 @@ const VideoDetail = ({ video }) => {
   return (
     <div className="relative">
       <iframe
-        id={`video-${video.id}`}
+        id={`video-${video?.id}`}
         loading="lazy"
-        className={`w-[90%] h-80 mx-auto my-[-50px] lg:bg-black`}
-        src={video.src}
+        className={`w-[90%] md:h-40 lg:h-80 mx-auto my-[-50px] lg:bg-black`}
+        src={video?.src}
         title="Vimeo video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
